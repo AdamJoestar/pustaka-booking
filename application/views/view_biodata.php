beeaaba746f1eea66805e491c9af828e4e910a1e
AdamJoestar <adamschoolauto@gmail.com>
2023-09-26 15:48:03 +0700

    Tugas Biodata Pertemuan 2

---
+++ application/views/view_biodata.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <title> Biodata Aku </title>
+    </head>
+    <body align="center">
+        <style>
+            body {
+                background:linear-gradient(90deg, Blue,White,Blue,White,Blue);
+            }
+        </style>
+        <table align="center">
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td>Adam Maualana</td>
+
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>:</td>
+                <td>10.3A.07</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>:</td>
+                <td>Rekayasa Perangkat Lunak</td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>Pancoran</td>
+            </tr>
+            <tr>
+                <td>No.Hp</td>
+                <td>:</td>
+                <td>082122725752</td>
+            </tr>
+        </table>
+    </body>
+</html>
