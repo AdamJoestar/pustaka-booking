134bcf44f1a6bd3dbac84b08131d2af629b3f262
AdamJoestar <adamschoolauto@gmail.com>
2023-10-10 15:18:04 +0700

    Tugas Form Validation

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -1,42 +1,45 @@
 <html>
     <head>
         <title>Tampil Data Matakuliah</title>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body>
         <center>
-            <table>
+            <table class="p-3 mb-2 bg-primary text-white">
                 <tr>
                     <th colspan="3">
                         Tampil Data Matakuliah
                     </th>
                 </tr>
-                <tr>
+                <tr >
                     <td colspan="3">
                         <hr>
                     </td>
                 </tr>
-                <tr>
+                <tr >
                     <th>Kode MTK</th>
                     <th>:</th>
                     <td>
                         <?= $kode; ?>
                     </td>
                 </tr>
-                <tr>
+                <tr >
                     <th>Nama MTK</th>
                     <th>:</th>
                     <td>
                         <?= $nama; ?>
                     </td>
                 </tr>
-                <tr>
+                <tr >
                     <th>SKS</th>
                     <th>:</th>
                     <td>
                         <?= $sks; ?>
                     </td>
                 </tr>
-                <tr>
+                <tr >
                     <td colspan="3" align="center">
                         <a href="<?= base_url('matakuliah');?>">Kembali</a>
                     </td>
